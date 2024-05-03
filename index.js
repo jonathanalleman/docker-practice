73e58277f62944e3fb20eca270c6e2a109686dc8
jonathanalleman <jonathanalleman@gmail.com>
2024-05-02 20:15:10 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const app = express()
+const port = 8080
+
+app.use(express.json())
+app.use('/',(req, res)=>{
+    res.send({status:200, message:"Hello World!"})
+})
+app.listen(port, ()=>{
+    console.log(`exapmle app listening at http://localhost:${port}`)
+})
